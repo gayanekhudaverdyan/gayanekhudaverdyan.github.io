f03ac0b50008525855005dbc567ec0acbd180c27
david.khudaverdyan <d.khudaverdyan@plaxis.com>
2019-07-09 17:27:51 +0200

    add menu.js

---
+++ menu.js
@@ -0,0 +1,14 @@
+  $( document ).ready(function() 
+  {
+    var htmlCode = `
+      <table class="header">
+        <tr>
+          <td class="header" bgcolor="#000000"><a class = "header" href="../">Главная&nbsp;страница</a></td>
+          <td bgcolor="#000000"></td>
+        </tr>
+      </table>
+    `;
+	$( "#menu" ).html
+	(htmlCode);
+  
+  });
