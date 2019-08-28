dde9fb2c9713e6bce4d30c349e6c5dbe9eb979ca
David <khdavid@mail.ru>
2019-08-28 14:54:27 +0300

    add size converter

--- python/resizePics.py
+++ python/resizePics.py
@@ -2,18 +2,46 @@ from PIL import Image
 import os
 import shutil
 
-rootDir = r'C:\Users\khuda\Desktop\gayanekhudaverdyan.github.io'
+rootDir = r'../pelmeni/pics'
+cMaxSizeImg = 679;
+cMaxWidthThumbNail = 250;
+cThumbNailFileName = '12.jpg'
 
-dirs = next(os.walk(rootDir))[1]
-for dir in dirs:
-  dir = os.path.join(rootDir, dir)
-  target = os.path.join(dir, 'main.jpg')
-  if os.path.exists(target):
-    targetConverted = os.path.join(dir, 'main_thumbnail.jpg')
-    baseheight = 186  
-    img = Image.open(target)
-    wpercent = (baseheight/float(img.size[1]))
-    hwidth = int((float(img.size[0])*float(wpercent)))
-    img = img.resize((hwidth,baseheight), Image.ANTIALIAS)
-    print(targetConverted)
-    img.save(targetConverted) 
+def getNewWidthHeight(img, maxSize):
+  if (img.size[0] > img.size[1]):
+    ratio = maxSize / img.size[0]
+    return [int(maxSize), int(ratio * img.size[1])]
+  else:
+    ratio = maxSize / img.size[1]
+    return [int(ratio * img.size[0]), maxSize]
+
+def getNewWidthHeightFixedWidth(img, width):
+    ratio = width / img.size[0]
+    return [int(width), int(ratio * img.size[1])]
+    
+os.mkdir(os.path.join(rootDir, "converted"))
+    
+[subdir, dirs, files] = next(os.walk(rootDir))
+print(files)
+i = 0
+for file in files:
+  if file.endswith('.jpg'):
+    filePath = os.path.join(subdir, file)
+    print(filePath)
+    img = Image.open(filePath)
+    sizes = getNewWidthHeight(img, cMaxSizeImg)
+    img = img.resize((sizes[0],sizes[1]), Image.ANTIALIAS)
+    i+=1
+    filePathTo = os.path.join(subdir,'converted', 'image' + str(i) + '.jpg')
+    print(filePathTo)
+    img.save(filePathTo) 
+    
+filePath = os.path.join(rootDir, cThumbNailFileName)
+print(filePath)
+img = Image.open(filePath)
+sizes = getNewWidthHeightFixedWidth(img, cMaxWidthThumbNail)
+img = img.resize((sizes[0],sizes[1]), Image.ANTIALIAS)
+filePathTo = os.path.join(rootDir, 'converted', 'main_thumbnail.jpg')
+print(filePathTo)
+img.save(filePathTo) 
+    
