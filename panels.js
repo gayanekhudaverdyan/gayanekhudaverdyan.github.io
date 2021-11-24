d68a87c55897c9c9b678940641fa57eeb7582a19
David Khudaverdyan <david.khudaverdyan@bentley.com>
2021-11-24 23:05:43 +0100

    Add categories in the main page

--- panels.js
+++ panels.js
@@ -1,106 +1,11 @@
-var links =
+﻿var links =
 [
-  ["adjika", "Аджика"],
-  ["harisa", "Ариса (разваренная каша из пшеницы и курицы)"],
-  ["achma", "Ачма (разновидность хачапури)"],
-  ["jarenye_baklajany", "Баклажаны жареные"],
-  ["bamia", "Бамия"],
-  ["barania_noga", "Баранья нога, запеченная в духовке"],
-  ["bezdrojevoe_sloenoe_testo", "Бездрожжевое слоеное тесто"],
-  ["beefstroganoff", "Бефстроганов"],
-  ["bliny", "Блинчики"],
-  ["biskvitny_tort", "Бисквитный торт"],
+  ["title", "Cупы"],
   ["bozbash", "Бозбаш"],
   ["borshtch", "Борщ"],
-  ["brokkoli_v_jaicakh", "Брокколи в яйцах и панировочных сухарях"],
-  ["bujenina", "Буженина из индейки или свинины"],
-  ["jareny_vermishel_lapsha", "Вермишель жареная"],
-  ["vinigret_kolkhozny", "Винегрет колхозный"],
-  ["garni_yarakh", "Гарни-ярах (вспоротое брюхо - жаренные баклажаны начиненные жаренным мясным фаршем)"],
-  ["gata", "Гата (слоеное тесто с начинкой из муки, масла и сахара или меда)"],
-  ["glazur_shokoladaia", "Глазурь шоколадная"],
-  ["goviadina_v_shampinione", "Говядина с шампиньонами в сметанном соусе"],
-  ["golubcy_s_kapustoi", "Голубцы с капустой"],
   ["gorokhovy_sup", "Гороховый суп с копченой индейкой"],
-  ["grenki", "Гренки"],
   ["gribnoi_sup", "Грибной суп"],
-  ["dolma", "Долма"],
-  ["jajik", "Жажик"],
-  ["jarenaia_ryba_v_kliare", "Жареная рыба в кляре"],
-  ["jarenye_baklajany_bolgarsky_perec", "Жареные баклажаны, болгарский перец и помидоры"],
-  ["jarkoe", "Жаркое"],
-  ["zakuska_iz_marinovannykh_zelenykh_pomidorov", "Закуска из маринованных зеленых помидоров"],
-  ["zaruska_iz_pecheni_treski", "Закуска из печени трески"],
-  ["zakuska_iz_pechenikh_baklajan", "Закуска из печеных баклажан"],
-  ["zelenaia_fasol", "Зеленая фасоль с лукок и яйцами"],
-  ["jarenie_kabachki", "Кабачки жареные"],
-  ["kartoshka_v_dukhovke", "Как пожарить картошку в духовке так, чтобы она не осталась сырой внутри"],
-  ["keks", "Кекс"],
-  ["keksovoe_testo", "Кексовое тесто"],
-  ["keks_bananovy", "Кекс банановый"],
-  ["keks_shokoladny", "Кекс шоколадный"],
-  ["keks_shokoladny_s_abrikosovym_djemom", "Кекс шоколадный с абрикосовым джемом"],
-  ["kotlety", "Котлеты"],
-  ["kotlomyshki", "Котломышки"],
-  ["krem_nuga", "Крем нуга"],
-  ["krem_poluzavarnoi_poluslivochny", "Крем полузаварной-полусливочный (идеал)"],
-  ["krushen", "Крюшон (легкий алкогольный напиток)"],
-  ["kubete", "Кубете (пирог из грубого слоеного теста с сырой начинкой из мяса, картошки и лука)"],
-  ["kuku", "Куку (пирог с зеленью, шпинатом и яйцами)"],
-  ["kulichi", "Куличи"],
-  ["kurinye_grudki_v_kliare", "Куриные грудки в кляре"],
-  ["kurica_s_pivom", "Курица, замаринованная в луке, политая пивом"],
-  ["kurica_zamarinovannaia", "Курица, замаринованная в луке, политая сметаной"],
-  ["letnija_zakuska", "Летняя закуска из печенных овощей"],
-  ["makarony_po_flotsky", "Макароны по-флотски"],
-  ["marinovka", "Мариновка (баранина, свинина, индейка, курица)"],
-  ["medovye_korji", "Медовые коржи"],
-  ["medovy_biskvit_nuga", "Медовый бисквит Нуга"],
-  ["mikado", "Микадо"],
-  ["molochny_sup_katnov", 'Молочный суп с рисом "катнов"'],
-  ["morkovny_keks", "Морковный кекс"],
-  ["morkovny_salat", "Морковный салат с грецкими орехами"],
-  ["myaso_s_ovoshami", "Мясо с овощами"],
-  ["olivie", "Оливье"],
-  ["otbivnye_ili_shitsel_iz_svininy", "Отбивные или шницель из свинины"],
-  ["pasuc_dolma", "Пасуц долма (постные голубнцы)"],
-  ["pahlava_sochinskaia", "Пахлава сочинская (из рубленного теста с начинкой из орех)"],
-  ["pashtet_is_pecheni", "Паштет из печени"],
-  ["pashtet_sup_is_krasoi_fasoli", "Паштет и суп из красной фасоли"],
-  ["pelmeni", "Пельмени"],
-  ["pesochonoe_testo_dlia_korzinochek", "Песочное тесто для корзиночек"],
-  ["pechenie_iz_detstva", "Печенье из нашего детства"],
-  ["pechenye_pirojki", "Печеные пирожки из дрожжевого теста"],
-  ["pechenye_pirojki_iz_kombinirovannogo_testa", "Печеные пирожки из комбинированного теста"],
-  ["pirojki", "Пирожки"],
-  ["piti", "Пити (густой суп из баранины)"],
-  ["plov_s_polboi", 'Плов с полбой и грибами "атчаров плов"'],
-  ["ponchiki_s_zavarnym_kremom", "Пончики с заварным кремом"],
-  ["ptiche_moloko", "Птичье молоко"],
-  ["pure_kartofelnoe", "Пюре картофельное"],
-  ["ratatui", "Рататуй"],
-  ["risovy_plov", "Рисовый плов"],
-  ["risovy_plov_s_kartoshkoi", "Рисовый плов с молодой картошкой"],
-  ["risovy_plov_sukhofrukty", "Рисовый плов с сухофруктами"],
-  ["risovy_plov_s_chechevicei", "Рисовый плов с чечевицей"],
-  ["ruletiki_iz_baklajan", "Рулетики из баклажан"],
-  ["ryba", "Рыба вареная"],
-  ["rybnye_konservy", "Рыбные консервы"],
-  ["rybny_sup", "Рыбный суп, или уха"],
-  ["salat", "Салат"],
-  ["salat_iz_kurinykh_grudok", "Салат из куриных грудок"],
-  ["salat_iz_marinovannykh_masliat", "Салат из маринованных маслят"],
-  ["salat_c_chechevicej", "Салат с чечевицей"],
-  ["sarakh", "Сарах (тонкое тесто для чебурек, начиненное картофельным пюре с пассированным луком)"],
-  ["sacivi", "Сациви (закуска из курицы, грецких орехов и специй)"],
-  ["svekolny_salat", "Свекольный салат"],
-  ["sigarety", "Сигареты"],
-  ["smes_so_specijami", "Смесь со специями"],
-  ["solenie", "Соленье"],
-  ["solianka", "Солянка"],
-  ["sous", "Соус"],
   ["spas", "Спас (мацони с пшеницей)"],
-  ["suzme_pelmeni", "Сузме (пельмени, заправленные подливой из грецких орехов)"],
   ["sup_krchik", "Суп крчик (квашенная капуста, морковь и пшеница)"],
   ["sup_ovoshnoi", "Суп овощной"],
   ["sup_rassolnik", "Суп рассольник"],
@@ -109,97 +14,220 @@ var links =
   ["sup_khashborek", "Суп хашборек"],
   ["sup_pure", "Суп-пюре"],
   ["supe_pure_s_syrom", "Суп-пюре с сыром"],
-  ["sufle", "Суфле"],
+  ["molochny_sup_katnov", 'Молочный суп с рисом "катнов"'],
+  ["pashtet_sup_is_krasoi_fasoli", "Паштет и суп из красной фасоли"],
+  ["piti", "Пити (густой суп из баранины)"],
+  ["rybny_sup", "Рыбный суп, или уха"],
+  ["sous", "Соус"],
+  ["chkhrtma", "Чхртма (грузинский суп с курицей)"],
+  ["khash", "Хаш (наваристый суп из говяжьих копыт и коленного сустава)"],
+  ["shavel_sup", "Щавелевый суп"],
+  
+  ["title", "Закуски и салаты"],
+  ["jajik", "Жажик"],
+  ["vinigret_kolkhozny", "Винегрет колхозный"],
+  ["zakuska_iz_marinovannykh_zelenykh_pomidorov", "Закуска из маринованных зеленых помидоров"],
+  ["zaruska_iz_pecheni_treski", "Закуска из печени трески"],
+  ["zakuska_iz_pechenikh_baklajan", "Закуска из печеных баклажан"],
+  ["letnija_zakuska", "Летняя закуска из печенных овощей"],
+  ["morkovny_salat", "Морковный салат с грецкими орехами"],
+  ["olivie", "Оливье"],
+  ["pasuc_dolma", "Пасуц долма (постные голубнцы)"],
+  ["pashtet_is_pecheni", "Паштет из печени"],
+  ["ruletiki_iz_baklajan", "Рулетики из баклажан"],
+  ["rybnye_konservy", "Рыбные консервы"],
+  ["salat", "Салат"],
+  ["salat_iz_kurinykh_grudok", "Салат из куриных грудок"],
+  ["salat_iz_marinovannykh_masliat", "Салат из маринованных маслят"],
+  ["salat_c_chechevicej", "Салат с чечевицей"],
+  ["sacivi", "Сациви (закуска из курицы, грецких орехов и специй)"],
+  ["svekolny_salat", "Свекольный салат"],
+  ["solenie", "Соленье"],
+  ["farshirovannaia_ryba_iz_karpa", "Фаршированный карп"],
+  ["farshirovany_perec", "Фаршированный перец"],
+  ["fruktovy_salat", "Фруктовый салат"],
+  ["kholodnia_zakuska_fasol", "Холодная закуска из зеленой фасоли"],
+  ["kholodnaia_zakuska_is_jazyka", "Язык (холодная закуска)"],
   ["tabule", "Табуле"],
+  ["jaica_po_evreiski", "Яйца по-еврейски"],
+  
+  ["title", "Горячее"],
+  ["harisa", "Ариса (разваренная каша из пшеницы и курицы)"],
+  ["barania_noga", "Баранья нога, запеченная в духовке"],
+  ["beefstroganoff", "Бефстроганов"],
+  ["bujenina", "Буженина из индейки или свинины"],
+  ["ryba", "Рыба вареная"],
+  ["garni_yarakh", "Гарни-ярах (вспоротое брюхо - жаренные баклажаны начиненные жаренным мясным фаршем)"],
+  ["goviadina_v_shampinione", "Говядина с шампиньонами в сметанном соусе"],
+  ["golubcy_s_kapustoi", "Голубцы с капустой"],
+  ["dolma", "Долма"],
+  ["jarenaia_ryba_v_kliare", "Жареная рыба в кляре"],
+  ["jarkoe", "Жаркое"],
+  ["kartoshka_v_dukhovke", "Как пожарить картошку в духовке так, чтобы она не осталась сырой внутри"],
+  ["kotlety", "Котлеты"],
+  ["kuku", "Куку (пирог с зеленью, шпинатом и яйцами)"],
+  ["kurinye_grudki_v_kliare", "Куриные грудки в кляре"],
+  ["kurica_s_pivom", "Курица, замаринованная в луке, политая пивом"],
+  ["kurica_zamarinovannaia", "Курица, замаринованная в луке, политая сметаной"],
+  ["uzbekski_plov", "Узбекский плов"],
+  ["makarony_po_flotsky", "Макароны по-флотски"],
+  ["marinovka", "Мариновка (баранина, свинина, индейка, курица)"],
+  ["myaso_s_ovoshami", "Мясо с овощами"],
+  ["pelmeni", "Пельмени"],
+  ["ratatui", "Рататуй"],
+  ["solianka", "Солянка"],
+  ["suzme_pelmeni", "Сузме (пельмени, заправленные подливой из грецких орехов)"],
+  ["khinkali", "Хинкали"],
+  ["khop_khop", "Хо-хоп"],
+  ["otbivnye_ili_shitsel_iz_svininy", "Отбивные или шницель из свинины"],
+  ["chakhokhbili", "Чахохбили из курицы"],
+  ["jaichnica_s_pomidorami", "Яичница с помидорами"],
+
+  ["title", "Гарниры"],
+  ["bamia", "Бамия"],
+  ["jarenye_baklajany", "Баклажаны жареные"],
+  ["brokkoli_v_jaicakh", "Брокколи в яйцах и панировочных сухарях"],
+  ["jareny_vermishel_lapsha", "Вермишель жареная"],
+  ["jarenye_baklajany_bolgarsky_perec", "Жареные баклажаны, болгарский перец и помидоры"],
+  ["zelenaia_fasol", "Зеленая фасоль с лукок и яйцами"],
+  ["jarenie_kabachki", "Кабачки жареные"],
+  ["plov_s_polboi", 'Плов с полбой и грибами "атчаров плов"'],
+  ["pure_kartofelnoe", "Пюре картофельное"],
+  ["risovy_plov", "Рисовый плов"],
+  ["risovy_plov_s_kartoshkoi", "Рисовый плов с молодой картошкой"],
+  ["risovy_plov_sukhofrukty", "Рисовый плов с сухофруктами"],
+  ["risovy_plov_s_chechevicei", "Рисовый плов с чечевицей"],
+  ["kapama_tykvennaia_kasha", 'Тыквенная каша с рисом "капама"'],
+  ["cvetnaja_kapusta_jarenaja", "Цветная капуста жареная"],
+  ["spinat_s_jaicom", "Шпинат с яйцом"],
+
+
+  ["title", "Выпечка и десерты"],
+  ["achma", "Ачма (разновидность хачапури)"],
+  ["bezdrojevoe_sloenoe_testo", "Бездрожжевое слоеное тесто"],
+  ["bliny", "Блинчики"],
+  ["biskvitny_tort", "Бисквитный торт"],
+  ["gata", "Гата (слоеное тесто с начинкой из муки, масла и сахара или меда)"],
+  ["glazur_shokoladaia", "Глазурь шоколадная"],
+  ["grenki", "Гренки"],
+  ["keks", "Кекс"],
+  ["keksovoe_testo", "Кексовое тесто"],
+  ["keks_bananovy", "Кекс банановый"],
+  ["keks_shokoladny", "Кекс шоколадный"],
+  ["keks_shokoladny_s_abrikosovym_djemom", "Кекс шоколадный с абрикосовым джемом"],
+  ["kotlomyshki", "Котломышки"],
+  ["krem_nuga", "Крем нуга"],
+  ["krem_poluzavarnoi_poluslivochny", "Крем полузаварной-полусливочный (идеал)"],
+  ["kubete", "Кубете (пирог из грубого слоеного теста с сырой начинкой из мяса, картошки и лука)"],
+  ["kulichi", "Куличи"],
+  ["medovye_korji", "Медовые коржи"],
+  ["medovy_biskvit_nuga", "Медовый бисквит Нуга"],
+  ["mikado", "Микадо"],
+  ["morkovny_keks", "Морковный кекс"],
+  ["pahlava_sochinskaia", "Пахлава сочинская (из рубленного теста с начинкой из орех)"],
+  ["pesochonoe_testo_dlia_korzinochek", "Песочное тесто для корзиночек"],
+  ["pechenie_iz_detstva", "Печенье из нашего детства"],
+  ["pechenye_pirojki", "Печеные пирожки из дрожжевого теста"],
+  ["pechenye_pirojki_iz_kombinirovannogo_testa", "Печеные пирожки из комбинированного теста"],
+  ["pirojki", "Пирожки"],
+  ["ponchiki_s_zavarnym_kremom", "Пончики с заварным кремом"],
+  ["ptiche_moloko", "Птичье молоко"],
+  ["sarakh", "Сарах (тонкое тесто для чебурек, начиненное картофельным пюре с пассированным луком)"],
+  ["sigarety", "Сигареты"],
+  ["sufle", "Суфле"],
   ["tvorojnoe_pechenie", "Творожное печенье"],
   ["testo_dlia_piroga", "Тесто для пирога"],
   ["testo_medovogo_biskvita", "Тесто медового бисквита"],
-  ["tkemali", "Ткемали"],
   ["tort_jozephina", "Торт Жозефина"],
   ["tort_medovoe_so_slivkami", 'Торт или пироженое "медовое со сливками"'],
-  ["kapama_tykvennaia_kasha", 'Тыквенная каша с рисом "капама"'],
-  ["uzbekski_plov", "Узбекский плов"],
-  ["farshirovannaia_ryba_iz_karpa", "Фаршированная рыба приготовленная из карпа"],
-  ["farshirovany_perec", "Фаршированный перец"],
-  ["fruktovy_salat", "Фруктовый салат"],
   ["khachapuri", "Хачапури"],
-  ["khash", "Хаш (наваристый суп из говяжьих копыт и коленного сустава)"],
   ["khashlama", "Хашлама"],
   ["khaslama_pikantnaia", "Хашлама пикантная"],
   ["khaslama_s_pomidorami_i_pivom", "Хашлама с помидорами и пивом"],
-  ["khinkali", "Хинкали"],
-  ["kholodnia_zakuska_fasol", "Холодная закуска из зеленой фасоли"],
-  ["khop_khop", "Хо-хоп"],
-  ["cvetnaja_kapusta_jarenaja", "Цветная капуста жареная"],
-  ["chakhokhbili", "Чахохбили из курицы"],
   ["chebureki", "Чебуреки"],
   ["cheche", "Чече (пирожки из грубого слоеного теста, начиненеые сырым мясным фаршем)"],
-  ["chkhrtma", "Чхртма (грузинский суп с курицей)"],
   ["sharlotka", "Шарлотка"],
   ["shokolady_keks", "Шоколадные кексы с семенами льна"],
   ["shokoladny_biskvit", "Шоколадный бисквит"],
   ["shokoladny_krem_belochka", "Шоколадный крем белочка"],
-  ["spinat_s_jaicom", "Шпинат с яйцом"],
-  ["shavel_sup", "Щавелевый суп"],
   ["eklery_ili_shu", "Эклеры или шу"],
-  ["kholodnaia_zakuska_is_jazyka", "Язык (холодная закуска)"],
-  ["jaichnica_s_pomidorami", "Яичница с помидорами"],
-  ["packh_jaica", "Яйца пасхальные"],
-  ["jaica_po_evreiski", "Яйца по-еврейски"]
+
+
+  ["title", "И что-то еще..."],
+  ["adjika", "Аджика"],
+  ["krushen", "Крюшон (легкий алкогольный напиток)"],
+  ["smes_so_specijami", "Смесь со специями"],
+  ["tkemali", "Ткемали"],
+  ["packh_jaica", "Яйца пасхальные"]
 ];
 
-  function generatePanelHTMLCode(link, name, imageLink)
-  {
-    var HTMLCode = `
-    <a class="panel" href="` + link + `/">
-      <div class="panel">
-        <div class="panelText">` + 
-          name +
-       `</div>
-        <div class="panelImg">
-          <img class="panel" src="` + imageLink + `">` +
-       `</div>
-     </div>
-    </a>
-    `
-    
-    return HTMLCode;
-
-  }
+function generatePanelHTMLCode(link, name, imageLink)
+{
+  var HTMLCode = `
+  <a class="panel" href="` + link + `/">
+    <div class="panel">
+      <div class="panelText">` + 
+        name +
+     `</div>
+      <div class="panelImg">
+        <img class="panel" src="` + imageLink + `">` +
+     `</div>
+   </div>
+  </a>
+  `
   
-  function generatePanels()
+  return HTMLCode;
+}
+
+function generateTitleHTMLCode(name)
+{
+  var HTMLCode = `
+  <div class="panelsGroupTitle">` + name + ` </div>
+  `
+  
+  return HTMLCode;
+}
+
+function generatePanels()
+{
+  var htmlCode = "";
+  for (var i in links)
   {
-    var htmlCode = "";
-    for (var i in links)
+    if (links[i][0] == "title")
+    {
+      htmlCode += generateTitleHTMLCode(links[i][1]);
+    }
+    else
     {
       htmlCode += generatePanelHTMLCode(
         links[i][0], links[i][1], links[i][0]+'/main_thumbnail.jpg');
     }
-    $("#panels").html(htmlCode);
   }
+  $("#panels").html(htmlCode);
+}
 
-  function updateCSS()
+function updateCSS()
+{
+  var width = $(window).width();
+  if (width < 400)
   {
-    var width = $(window).width();
-    if (width < 400)
-    {
-      $("div.panel").css('width', "45%");
-    }
-    else
-    {
-      $("div.panel").css('width', "170px");
-    }
+    $("div.panel").css('width', "45%");
   }
+  else
+  {
+    $("div.panel").css('width', "170px");
+  }
+}
 
-  $( document ).ready(function() 
-  { 
-    generatePanels();
-    updateCSS();
-  }
-  );
-  
-    $( window ).resize(function() 
-  {
-    updateCSS();
-  }
-  );
+$( document ).ready(function() 
+{ 
+  generatePanels();
+  updateCSS();
+}
+);
+
+  $( window ).resize(function() 
+{
+  updateCSS();
+}
+);
